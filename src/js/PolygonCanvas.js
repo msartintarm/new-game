ea0446b12e5cab24c79767e9aebcabe6821f8652
Michael Sartin-Tarm <msartintarm@gmail.com>
2016-05-31 23:05:22 -0700

    line end key is working. sorted out event target issues by binding to document

--- src/js/PolygonCanvas.js
+++ src/js/PolygonCanvas.js
@@ -21,6 +21,26 @@ const keyCodeMap = {
 	40: 'Down'
 };
 
+const handlerList = [
+	[ 'keydown', 'keyDownHandler'],
+	[ 'mousedown', 'mouseDownHandler'],
+	[ 'mousemove', 'mouseMoveHandler'],
+	[ 'mouseover', 'mouseOverHandler'],
+	[ 'touchdown', 'touchDownHandler']
+];
+
+
+class DisplayArray extends React.Component {
+	render () {
+		return(
+			<div className="display_array" >
+				<b className="display_label">Coords for current line</b>
+				{this.props.text}
+			</div>
+		);
+	}
+}
+
 /* Tells canvas what to draw */
 class PolygonCanvas extends React.Component {
 
@@ -33,14 +53,19 @@ class PolygonCanvas extends React.Component {
 		this.state = ({ 
 			button_text: this.getStartButtonText('line end', this.keyList['line end']),
 			polygon_arr: [[]],
-			polygon_arr_text: ''
+			polygon_arr_text: 'no coords bro',
+			example_line: [],
+			example_line_text: 'no coords bro',
+			mouseDownElem: this
 		});
 		// ES6 React auto bind alternatives:
 		// http://egorsmirnov.me/2015/08/16/react-and-es6-part3.html
-	
-		this.keyDownHandler = this.keyDownHandler.bind(this);
-		this.mouseDownHandler = this.mouseDownHandler.bind(this);
-		this.touchDownHandler = this.touchDownHandler.bind(this);
+
+		for (let [key, val] of handlerList) {
+			this[val] = this[val].bind(this);
+			console.log(key + "," + val);
+			document.addEventListener(key, this[val]);
+		}
 
 		this.setKey = {
 			'line end': this.setKeyGen('line end')
@@ -92,36 +117,73 @@ class PolygonCanvas extends React.Component {
 	}
 
 	onButtonMouseDown (e) {
-		this.setState({ button_text: 'next key down..' });
+		var newState = { button_text: 'next key down..' }; 
+		this.recordMouseDown(e, newState);
+		this.setState(newState);
 		document.body.addEventListener('keydown', this.setKey['line end']);
 	}
 
 	/* Update polygon array with point from latest canvas click. */
 	onCanvasMouseDown (e) {
-
 		let newArr = [...this.state.polygon_arr]; // make copy
 		newArr[0].push(this.getCoords(e));
-
 		let newState = {
 			polygon_arr: newArr,
 			polygon_arr_text: JSON.stringify(newArr)
 		};
+		this.recordMouseDown(e, newState);
 
 		this.setState(newState);
+	}
+
+	/* Cut off this line. Only for a given key and if canvas was last clicked
+		Takes it directly from last draw of example line */
+	onCanvasKeyDown (e) {
+		if (this.getElemKey(this.state.mouseDownElem) !== 'CANVAS') { return; }
+		if (e.keyCode !== this.keyList['line end']) { return; }
+		if (this.state.example_line.length < 2) { return; }
+		let newArr = [[], ...this.state.polygon_arr]; // add entry
+		newArr[1].push([...this.state.example_line[1]]);
+		let newState = {
+			polygon_arr: newArr,
+			polygon_arr_text: JSON.stringify(newArr),
+			example_line: [],
+			example_line_text: 'no coords bro'
+		};
+		this.setState(newState);
+	}
+
+	/* record mouse down; if called with new state, append to it instead */
+	recordMouseDown (e, newState) {
+		if (!newState) {
+			this.setState({ mouseDownElem: e.target });
+		} else {
+			newState.mouseDownElem = e.target;
+		}
+		console.log(e.target);
+		console.log(e.target.className);
 	}
 
 	/* Draws example line with last point */
-	onCanvasMouseOver (e) {
-		if (!this.state.polygon_arr.length > 1) { return; }
-
+	onCanvasMouseMove (e) {
+		if (this.state.polygon_arr.length < 1) { return; }
+		let theArr = this.state.polygon_arr[0];
+		if (theArr.length < 1) { return; }
+		let newArr = [
+			[...theArr[theArr.length - 1]], // copy last coord in line
+			this.getCoords(e)
+		];
 		let newState = {
-			example_line: [
-				this.state.polygon_arr[0],
-				[ e.pageX, e.pageY ]
-		]};
-
+			example_line: newArr,
+			example_line_text: JSON.stringify(newArr)
+		};
 		this.setState(newState);
 	}
+
+	/* Todo: something cool */
+	onCanvasMouseOver (e) {}
+
+	getElemKey (elem) { return elem.className || elem.tagName; }
 
 	/* Looks for handler defined in mapping
 		- Checks in order 
@@ -129,25 +191,36 @@ class PolygonCanvas extends React.Component {
 		  2. target element tag name
 	*/
 	handler(e, map) {
-		let target = e.target;
-		let mapKey = target.className || target.tagName;
+		let mapKey = this.getElemKey(e.target);
 		let fn = map[mapKey];
-		return fn? fn.call(this, e): null;
+		return fn? fn.call(this, e): 
+			map['default']? map['default'].call(this, e): null;
 	}
 
-	mouseDownHandler (e) { return this.handler(e, {
-			"CANVAS": this.onCanvasMouseDown,
-			"line_end_button": this.onButtonMouseDown
+	mouseOverHandler (e) { this.handler(e, {
+			"CANVAS": this.onCanvasMouseOver
 		});
 	}
 
-	touchDownHandler (e) { return this.handler(e, {
+	mouseMoveHandler (e) { this.handler(e, {
+			"CANVAS": this.onCanvasMouseMove
+		});
+	}
+
+	mouseDownHandler (e) { this.handler(e, {
+			"CANVAS": this.onCanvasMouseDown,
+			"line_end_button": this.onButtonMouseDown,
+			default: this.recordMouseDown
+		});
+	}
+
+	touchDownHandler (e) { this.handler(e, {
 			"CANVAS": this.onCanvasTouchDown 
 		});
 	}
-
-	keyDownHandler (e) { return this.handler(e, {
-			"CANVAS": this.onCanvasKeyDown 
+	
+	keyDownHandler (e) { this.handler(e, {
+			default: this.onCanvasKeyDown 
 		});
 	}
 
@@ -155,21 +228,17 @@ class PolygonCanvas extends React.Component {
 
 	render () {
 		return (
-		<div className="container"
-			onMouseDown={this.mouseDownHandler}
-			onTouchDown={this.touchDownHandler}
-			onKeyDown={this.keyDownHandler} >
+		<div className="container" >
 			<button className="line_end_button" >
 				{this.state.button_text} 
 			</button>
 			<div>
 				<TheCanvas className="canvas"
 					frameNum={this.props.frameNum}
-					lineSegments={[...this.state.polygon_arr, this.example_line ]} />
+					lineSegments={[...this.state.polygon_arr, this.state.example_line ]} />
 			</div>
-			<div className="polygon_arr_text" >
-				{this.state.polygon_arr_text}
-			</div>
+			<DisplayArray text={this.state.polygon_arr_text} />
+			<DisplayArray text={this.state.example_line_text} />
 		</div>
 		);
 	}
